2adb63a061cadc0e24735d3057c611950492a9ff
sarpisik <contact@sarpisik.com>
2020-01-23 12:44:30 +0100

    Add Service section

---
+++ src/components/Service.js
@@ -0,0 +1,76 @@
+import React from 'react';
+
+const Service = () => {
+  return (
+    <section className='wrapper'>
+      <div className='inner'>
+        <header className='special'>
+          <h2>Faucibus consequat lorem</h2>
+          <p>
+            In arcu accumsan arcu adipiscing accumsan orci ac. Felis id enim
+            aliquet. Accumsan ac integer lobortis commodo ornare aliquet
+            accumsan erat tempus amet porttitor.
+          </p>
+        </header>
+        <div className='testimonials'>
+          <section>
+            <div className='content'>
+              <blockquote>
+                <p>
+                  Nunc lacinia ante nunc ac lobortis ipsum. Interdum adipiscing
+                  gravida odio porttitor sem non mi integer non faucibus.
+                </p>
+              </blockquote>
+              <div className='author'>
+                <div className='image'>
+                  <img src='images/pic01.jpg' alt='' />
+                </div>
+                <p className='credit'>
+                  - <strong>Jane Doe</strong> <span>CEO - ABC Inc.</span>
+                </p>
+              </div>
+            </div>
+          </section>
+          <section>
+            <div className='content'>
+              <blockquote>
+                <p>
+                  Nunc lacinia ante nunc ac lobortis ipsum. Interdum adipiscing
+                  gravida odio porttitor sem non mi integer non faucibus.
+                </p>
+              </blockquote>
+              <div className='author'>
+                <div className='image'>
+                  <img src='images/pic03.jpg' alt='' />
+                </div>
+                <p className='credit'>
+                  - <strong>John Doe</strong> <span>CEO - ABC Inc.</span>
+                </p>
+              </div>
+            </div>
+          </section>
+          <section>
+            <div className='content'>
+              <blockquote>
+                <p>
+                  Nunc lacinia ante nunc ac lobortis ipsum. Interdum adipiscing
+                  gravida odio porttitor sem non mi integer non faucibus.
+                </p>
+              </blockquote>
+              <div className='author'>
+                <div className='image'>
+                  <img src='images/pic02.jpg' alt='' />
+                </div>
+                <p className='credit'>
+                  - <strong>Janet Smith</strong> <span>CEO - ABC Inc.</span>
+                </p>
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Service;
