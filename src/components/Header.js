6813db5801804bb5e8a4ac7db6389fd721bb27f6
sarpisik <contact@sarpisik.com>
2020-01-23 12:32:11 +0100

    Add Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import { Link } from 'gatsby';
-
-import Container from 'components/Container';
 
 const Header = () => {
   return (
-    <header>
-      <Container>
-        <p>
-          My Gatsby Site
-        </p>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2/">Page 2</Link>
-          </li>
-        </ul>
-      </Container>
+    <header id='header'>
+      <a class='logo' href='index.html'>
+        Industrious
+      </a>
+      <nav>
+        <a href='#menu'>Menu</a>
+      </nav>
     </header>
   );
 };
