ff994996f518b380fc4a0167320052421aa4ec73
sarpisik <contact@sarpisik.com>
2020-01-23 16:45:20 +0100

    Add GoogleMap.js component

---
+++ src/components/GoogleMap.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker
+} from 'react-google-maps';
+
+const Map = withScriptjs(
+  withGoogleMap(props => (
+    <GoogleMap
+      defaultZoom={12}
+      defaultCenter={{ lat: 37.004377, lng: 35.283982 }}>
+      {props.isMarkerShown && (
+        <Marker position={{ lat: 37.004377, lng: 35.283982 }} />
+      )}
+    </GoogleMap>
+  ))
+);
+export default Map;
