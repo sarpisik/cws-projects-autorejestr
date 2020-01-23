52d5aae0ec7462eb48d1d475e8949b95748385dc
sarpisik <contact@sarpisik.com>
2020-01-23 15:54:52 +0100

    Add scroll animate

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { animateScroll as scroll } from 'react-scroll';
 import toggleNav from '../lib/toggleNav';
 import Nav from './Nav';
 
 const Header = () => {
+  scrollToTop = () => {
+    scroll.scrollToTop();
+  };
   return (
     <header id='header'>
-      <a class='logo' href='index.html'>
+      <a className='logo' onClick={scrollToTop}>
         Industrious
       </a>
       <nav className='header-menu'>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import toggleNav from '../lib/toggleNav';
-import { Link, animateScroll as scroll } from 'react-scroll';
+import { Link } from 'react-scroll';
 
 const Nav = () => {
   return (
