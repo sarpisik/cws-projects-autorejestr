6516a117416bc1239eb965dbb0aeadbdfbc16849
sarpisik <contact@sarpisik.com>
2020-01-23 12:30:17 +0100

    Add sass

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,36 +2,34 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 
-import 'assets/stylesheets/application.scss';
+import 'assets/sass/main.scss';
 
 import Header from 'components/Header';
 import Footer from 'components/Footer';
 
 const Layout = ({ children, pageName }) => {
-
   let className = '';
 
-  if ( pageName ) {
+  if (pageName) {
     className = `${className} page-${pageName}`;
   }
 
   return (
     <>
-      <Helmet bodyAttributes={{ class: className}}>
+      <Helmet bodyAttributes={{ class: className }}>
         <title>Gatsby Site</title>
       </Helmet>
-      <div className="wrapper">
+      <div className='wrapper'>
         <Header />
-        <main>{ children }</main>
+        <main>{children}</main>
         <Footer />
       </div>
     </>
   );
-
 };
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+  children: PropTypes.node.isRequired
+};
 
 export default Layout;
