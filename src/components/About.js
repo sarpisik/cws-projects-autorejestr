bacdfd6d46a34022b671afc2bc0c60c0309f88d7
sarpisik <contact@sarpisik.com>
2020-01-23 16:46:58 +0100

    Add set content to google map

--- src/components/About.js
+++ src/components/About.js
@@ -1,19 +1,23 @@
 import React from 'react';
+import GoogleMap from './GoogleMap';
 
 const About = () => {
   return (
     <section id='cta' className='wrapper'>
       <div className='inner'>
-        <h2>Curabitur ullamcorper ultricies</h2>
-        <p>
-          Nunc lacinia ante nunc ac lobortis. Interdum adipiscing gravida odio
-          porttitor sem non mi integer non faucibus ornare mi ut ante amet
-          placerat aliquet. Volutpat eu sed ante lacinia sapien lorem accumsan
-          varius montes viverra nibh in adipiscing. Lorem ipsum dolor vestibulum
-          ante ipsum primis in faucibus vestibulum. Blandit adipiscing eu felis
-          iaculis volutpat ac adipiscing sed feugiat eu faucibus. Integer ac sed
-          amet praesent. Nunc lacinia ante nunc ac gravida.
-        </p>
+        <GoogleMap
+          isMarkerShown
+          googleMapURL={`https://maps.googleapis.com/maps/api/js?${
+            process.env.GATSBY_GOOGLE_MAPS_API
+              ? 'key=' + process.env.GATSBY_GOOGLE_MAPS_API + '&'
+              : ''
+          }v=3.exp&libraries=geometry,drawing,places`}
+          loadingElement={<div className='map' />}
+          containerElement={<div className='map' />}
+          mapElement={
+            <div className='column-radius map' style={{ height: `100%` }} />
+          }
+        />
       </div>
     </section>
   );
