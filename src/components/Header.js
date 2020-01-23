caf6d9d79614f2aac0019acf869315fc18700438
sarpisik <contact@sarpisik.com>
2020-01-23 19:29:36 +0100

    Add logo component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { animateScroll as scroll } from 'react-scroll';
 import toggleNav from '../lib/toggleNav';
 import Nav from './Nav';
+import logo from '../assets/images/logo_for_bg_black.svg';
 
 const Header = () => {
   const scrollToTop = () => {
@@ -10,7 +11,7 @@ const Header = () => {
   return (
     <header id='header'>
       <a className='logo' onClick={scrollToTop}>
-        Industrious
+        <img src={logo} alt='company logo' />
       </a>
       <nav className='header-menu'>
         <a onClick={toggleNav}>Menu</a>
