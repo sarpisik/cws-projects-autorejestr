54d998827f956fab8d284b21c41a0e4ecd04f720
sarpisik <contact@sarpisik.com>
2020-01-23 12:33:35 +0100

    Add Nav

---
+++ src/components/Nav.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <nav id='menu'>
+      <ul class='links'>
+        <li>
+          <a href='index.html'>Home</a>
+        </li>
+        <li>
+          <a href='elements.html'>Elements</a>
+        </li>
+        <li>
+          <a href='generic.html'>Generic</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
