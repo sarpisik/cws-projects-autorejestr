362534934bb549fa0b5b33b21cd1e5699738219b
sarpisik <contact@sarpisik.com>
2020-01-23 17:36:29 +0100

    Add remove section className

--- src/components/About.js
+++ src/components/About.js
@@ -3,22 +3,20 @@ import GoogleMap from './GoogleMap';
 
 const About = () => {
   return (
-    <section id='cta' className='wrapper'>
-      <div className='inner'>
-        <GoogleMap
-          isMarkerShown
-          googleMapURL={`https://maps.googleapis.com/maps/api/js?${
-            process.env.GATSBY_GOOGLE_MAPS_API
-              ? 'key=' + process.env.GATSBY_GOOGLE_MAPS_API + '&'
-              : ''
-          }v=3.exp&libraries=geometry,drawing,places`}
-          loadingElement={<div className='map' />}
-          containerElement={<div className='map' />}
-          mapElement={
-            <div className='column-radius map' style={{ height: `100%` }} />
-          }
-        />
-      </div>
+    <section id='cta'>
+      <GoogleMap
+        isMarkerShown
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?${
+          process.env.GATSBY_GOOGLE_MAPS_API
+            ? 'key=' + process.env.GATSBY_GOOGLE_MAPS_API + '&'
+            : ''
+        }v=3.exp&libraries=geometry,drawing,places`}
+        loadingElement={<div className='map' />}
+        containerElement={<div className='map' />}
+        mapElement={
+          <div className='column-radius map' style={{ height: `20rem` }} />
+        }
+      />
     </section>
   );
 };
