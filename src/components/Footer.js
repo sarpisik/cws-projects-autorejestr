1648922c1b031c72e70bff136c9e4334a3a0b8b1
sarpisik <contact@sarpisik.com>
2020-01-23 12:46:00 +0100

    Add change Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,70 @@ import Container from 'components/Container';
 
 const Footer = () => {
   return (
-    <footer>
-      <Container>
-        <p>&copy; {new Date().getFullYear()}, My Gatsby Site</p>
-      </Container>
+    <footer id='footer'>
+      <div className='inner'>
+        <div className='content'>
+          <section>
+            <h3>Accumsan montes viverra</h3>
+            <p>
+              Nunc lacinia ante nunc ac lobortis. Interdum adipiscing gravida
+              odio porttitor sem non mi integer non faucibus ornare mi ut ante
+              amet placerat aliquet. Volutpat eu sed ante lacinia sapien lorem
+              accumsan varius montes viverra nibh in adipiscing. Lorem ipsum
+              dolor vestibulum ante ipsum primis in faucibus vestibulum. Blandit
+              adipiscing eu felis iaculis volutpat ac adipiscing sed feugiat eu
+              faucibus. Integer ac sed amet praesent. Nunc lacinia ante nunc ac
+              gravida.
+            </p>
+          </section>
+          <section>
+            <h4>Sem turpis amet semper</h4>
+            <ul className='alt'>
+              <li>
+                <a href='#'>Dolor pulvinar sed etiam.</a>
+              </li>
+              <li>
+                <a href='#'>Etiam vel lorem sed amet.</a>
+              </li>
+              <li>
+                <a href='#'>Felis enim feugiat viverra.</a>
+              </li>
+              <li>
+                <a href='#'>Dolor pulvinar magna etiam.</a>
+              </li>
+            </ul>
+          </section>
+          <section>
+            <h4>Magna sed ipsum</h4>
+            <ul className='plain'>
+              <li>
+                <a href='#'>
+                  <i className='icon fa-twitter'>&nbsp;</i>Twitter
+                </a>
+              </li>
+              <li>
+                <a href='#'>
+                  <i className='icon fa-facebook'>&nbsp;</i>Facebook
+                </a>
+              </li>
+              <li>
+                <a href='#'>
+                  <i className='icon fa-instagram'>&nbsp;</i>Instagram
+                </a>
+              </li>
+              <li>
+                <a href='#'>
+                  <i className='icon fa-github'>&nbsp;</i>Github
+                </a>
+              </li>
+            </ul>
+          </section>
+        </div>
+        <div className='copyright'>
+          &copy; Untitled. Photos <a href='https://unsplash.co'>Unsplash</a>,
+          Video <a href='https://coverr.co'>Coverr</a>.
+        </div>
+      </div>
     </footer>
   );
 };
