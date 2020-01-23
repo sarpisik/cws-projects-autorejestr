4b593c6dd37c2d5a26ffb8e1bc41abed5d34055a
sarpisik <contact@sarpisik.com>
2020-01-23 12:34:07 +0100

    Add Banner

---
+++ src/components/Banner.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Banner = () => {
+  return (
+    <section id='banner'>
+      <div class='inner'>
+        <h1>Industrious</h1>
+        <p>
+          A responsive business oriented template with a video background
+          <br />
+          designed by <a href='https://templated.co/'>TEMPLATED</a> and released
+          under the Creative Commons License.
+        </p>
+      </div>
+      {/* <video autoplay loop muted playsinline src='images/banner.mp4'></video> */}
+    </section>
+  );
+};
+
+export default Banner;
