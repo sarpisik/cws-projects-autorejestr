e83a18208d5af132285a19f3f8481723b6a46d08
sarpisik <contact@sarpisik.com>
2020-01-25 00:17:50 +0100

    Add Work component

--- src/components/Works.js
+++ src/components/Works.js
@@ -1,106 +1,118 @@
 import React from 'react';
 
+const Work = ({ icon, title, description }) => {
+    return (
+        <section>
+            <div className='content'>
+                <header>
+                    <a href='#' className='icon fa-vcard-o'>
+                        <span className='label'>Icon</span>
+                    </a>
+                    <h3>Feugiat consequat</h3>
+                </header>
+                <p>
+                    Nunc lacinia ante nunc ac lobortis ipsum. Interdum
+                    adipiscing gravida odio porttitor sem non mi integer non
+                    faucibus.
+                </p>
+            </div>
+        </section>
+    );
+};
+
 const Works = () => {
-  return (
-    <section id='section2' className='wrapper'>
-      <div className='inner'>
-        <header className='special'>
-          <h2>Sem turpis amet semper</h2>
-          <p>
-            In arcu accumsan arcu adipiscing accumsan orci ac. Felis id enim
-            aliquet. Accumsan ac integer lobortis commodo ornare aliquet
-            accumsan erat tempus amet porttitor.
-          </p>
-        </header>
-        <div className='highlights'>
-          <section>
-            <div className='content'>
-              <header>
-                <a href='#' className='icon fa-vcard-o'>
-                  <span className='label'>Icon</span>
-                </a>
-                <h3>Feugiat consequat</h3>
-              </header>
-              <p>
-                Nunc lacinia ante nunc ac lobortis ipsum. Interdum adipiscing
-                gravida odio porttitor sem non mi integer non faucibus.
-              </p>
+    return (
+        <section id='section2' className='wrapper'>
+            <div className='inner'>
+                <header className='special'>
+                    <h2>Sem turpis amet semper</h2>
+                    <p>
+                        In arcu accumsan arcu adipiscing accumsan orci ac. Felis
+                        id enim aliquet. Accumsan ac integer lobortis commodo
+                        ornare aliquet accumsan erat tempus amet porttitor.
+                    </p>
+                </header>
+                <div className='highlights'>
+                    <Work />
+                    <section>
+                        <div className='content'>
+                            <header>
+                                <a href='#' className='icon fa-files-o'>
+                                    <span className='label'>Icon</span>
+                                </a>
+                                <h3>Ante sem integer</h3>
+                            </header>
+                            <p>
+                                Nunc lacinia ante nunc ac lobortis ipsum.
+                                Interdum adipiscing gravida odio porttitor sem
+                                non mi integer non faucibus.
+                            </p>
+                        </div>
+                    </section>
+                    <section>
+                        <div className='content'>
+                            <header>
+                                <a href='#' className='icon fa-floppy-o'>
+                                    <span className='label'>Icon</span>
+                                </a>
+                                <h3>Ipsum consequat</h3>
+                            </header>
+                            <p>
+                                Nunc lacinia ante nunc ac lobortis ipsum.
+                                Interdum adipiscing gravida odio porttitor sem
+                                non mi integer non faucibus.
+                            </p>
+                        </div>
+                    </section>
+                    <section>
+                        <div className='content'>
+                            <header>
+                                <a href='#' className='icon fa-line-chart'>
+                                    <span className='label'>Icon</span>
+                                </a>
+                                <h3>Interdum gravida</h3>
+                            </header>
+                            <p>
+                                Nunc lacinia ante nunc ac lobortis ipsum.
+                                Interdum adipiscing gravida odio porttitor sem
+                                non mi integer non faucibus.
+                            </p>
+                        </div>
+                    </section>
+                    <section>
+                        <div className='content'>
+                            <header>
+                                <a href='#' className='icon fa-paper-plane-o'>
+                                    <span className='label'>Icon</span>
+                                </a>
+                                <h3>Faucibus consequat</h3>
+                            </header>
+                            <p>
+                                Nunc lacinia ante nunc ac lobortis ipsum.
+                                Interdum adipiscing gravida odio porttitor sem
+                                non mi integer non faucibus.
+                            </p>
+                        </div>
+                    </section>
+                    <section>
+                        <div className='content'>
+                            <header>
+                                <a href='#' className='icon fa-qrcode'>
+                                    <span className='label'>Icon</span>
+                                </a>
+                                <h3>Accumsan viverra</h3>
+                            </header>
+                            <p>
+                                Nunc lacinia ante nunc ac lobortis ipsum.
+                                Interdum adipiscing gravida odio porttitor sem
+                                non mi integer non faucibus.
+                            </p>
+                        </div>
+                    </section>
+                </div>
             </div>
-          </section>
-          <section>
-            <div className='content'>
-              <header>
-                <a href='#' className='icon fa-files-o'>
-                  <span className='label'>Icon</span>
-                </a>
-                <h3>Ante sem integer</h3>
-              </header>
-              <p>
-                Nunc lacinia ante nunc ac lobortis ipsum. Interdum adipiscing
-                gravida odio porttitor sem non mi integer non faucibus.
-              </p>
-            </div>
-          </section>
-          <section>
-            <div className='content'>
-              <header>
-                <a href='#' className='icon fa-floppy-o'>
-                  <span className='label'>Icon</span>
-                </a>
-                <h3>Ipsum consequat</h3>
-              </header>
-              <p>
-                Nunc lacinia ante nunc ac lobortis ipsum. Interdum adipiscing
-                gravida odio porttitor sem non mi integer non faucibus.
-              </p>
-            </div>
-          </section>
-          <section>
-            <div className='content'>
-              <header>
-                <a href='#' className='icon fa-line-chart'>
-                  <span className='label'>Icon</span>
-                </a>
-                <h3>Interdum gravida</h3>
-              </header>
-              <p>
-                Nunc lacinia ante nunc ac lobortis ipsum. Interdum adipiscing
-                gravida odio porttitor sem non mi integer non faucibus.
-              </p>
-            </div>
-          </section>
-          <section>
-            <div className='content'>
-              <header>
-                <a href='#' className='icon fa-paper-plane-o'>
-                  <span className='label'>Icon</span>
-                </a>
-                <h3>Faucibus consequat</h3>
-              </header>
-              <p>
-                Nunc lacinia ante nunc ac lobortis ipsum. Interdum adipiscing
-                gravida odio porttitor sem non mi integer non faucibus.
-              </p>
-            </div>
-          </section>
-          <section>
-            <div className='content'>
-              <header>
-                <a href='#' className='icon fa-qrcode'>
-                  <span className='label'>Icon</span>
-                </a>
-                <h3>Accumsan viverra</h3>
-              </header>
-              <p>
-                Nunc lacinia ante nunc ac lobortis ipsum. Interdum adipiscing
-                gravida odio porttitor sem non mi integer non faucibus.
-              </p>
-            </div>
-          </section>
-        </div>
-      </div>
-    </section>
-  );
+        </section>
+    );
 };
 
 export default Works;
