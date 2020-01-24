c94da00c8eaa8a79cd767ecb7b8da67334f9bff6
sarpisik <contact@sarpisik.com>
2020-01-25 00:11:07 +0100

    Remove BackgroundImage className

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,24 +2,26 @@ import React from 'react';
 import BackgroundImage from 'gatsby-background-image-es5';
 
 const Banner = ({ image }) => {
-  return (
-    <BackgroundImage
-      id='banner'
-      Tag='section'
-      // className={className}
-      fluid={image}
-      backgroundColor={`#040e18`}>
-      <div className='inner'>
-        <h1>Industrious</h1>
-        <p>
-          A responsive business oriented template with a video background
-          <br />
-          designed by <a href='https://templated.co/'>TEMPLATED</a> and released
-          under the Creative Commons License.
-        </p>
-      </div>
-    </BackgroundImage>
-  );
+    return (
+        <BackgroundImage
+            id='banner'
+            Tag='section'
+            fluid={image}
+            backgroundColor={`#040e18`}>
+            <div className='inner'>
+                <h1>Industrious</h1>
+                <p>
+                    A responsive business oriented template with a video
+                    background
+                    <br />
+                    designed by <a href='https://templated.co/'>
+                        TEMPLATED
+                    </a>{' '}
+                    and released under the Creative Commons License.
+                </p>
+            </div>
+        </BackgroundImage>
+    );
 };
 
 export default Banner;
